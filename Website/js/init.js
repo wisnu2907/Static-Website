dd6cd876594735f9c5de3ab4f19c2ba005bb2960
Wisnu Imam <90234571+wisnu2907@users.noreply.github.com>
2024-03-14 11:42:38 +0700

    Add files via upload

---
+++ Website/js/init.js
@@ -0,0 +1,13 @@
+(function ($) {
+  $(function () {
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.materialboxed').materialbox();
+    $('.scrollspy').scrollSpy();
+    $('.fixed-action-btn').floatingActionButton();
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
